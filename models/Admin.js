cfe463053dfe7ad150d22473d4fa24673a314b7f
Daniel Opara <oparadaniv@gmail.com>
2024-10-08 22:48:25 +0100

    changes to admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,37 +1,41 @@
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const AdminSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: [true, 'Please provide email'],
+    required: [true, "Please provide email"],
     unique: true,
   },
   name: {
     type: String,
-    required: [true, 'Please provide username'],
+    required: [true, "Please provide username"],
     minlength: 3,
     maxlength: 50,
   },
   password: {
     type: String,
-    required: [true, 'Please provide password'],
+    required: [true, "Please provide password"],
     minlength: 8,
   },
   role: {
     type: String,
-    enum: ['admin', 'user'],
-    default: 'user',
+    enum: ["admin", "user"],
+    //default should be admin since user wont be used
+    default: "admin",
   },
 });
 
 // Hash password before saving
-AdminSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return;
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+AdminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 // Compare passwords during login
@@ -40,4 +44,4 @@ AdminSchema.methods.comparePassword = async function (candidatePassword) {
   return isMatch;
 };
 
-module.exports = mongoose.model('Admin', AdminSchema);
+module.exports = mongoose.model("Admin", AdminSchema);
